19eb507c0734cacd4e7875dd8b3b5efa166bdde7
svitetiana <svitetiana@gmail.com>
2022-08-12 21:14:14 +0300

    Integrated humitidy and wind speed data

--- source/script.js
+++ source/script.js
@@ -3,9 +3,13 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "2e8a4b45b375acbabf901bd141fc3608";
