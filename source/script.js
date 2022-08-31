08ee09e5380cd16cf36642ed7e37283df83bc568
svitetiana <svitetiana@gmail.com>
2022-08-31 19:38:44 +0300

    Added layout for the weather forecast

--- source/script.js
+++ source/script.js
@@ -21,6 +21,29 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+         <div class="weather-forecast-date">${day}</div>
+           <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">18</span>
+              <span class="weather=forecast-temperature-min">12</span>
+          </div>
+         </div>
+      
+     `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -86,3 +109,4 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("Lviv");
+displayForecast();
